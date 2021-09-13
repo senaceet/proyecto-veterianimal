4d5c1d2d63bace58c3ffe07a59249b416d984f4f
Luisa Mendieta <69684179+LuisaMendieta-15@users.noreply.github.com>
2021-09-12 19:24:52 -0500

    docs: "actualizacion del proyecto, se agregan algunos htmls "

--- PROYECTO/Veterianimal/Veterianimal/Veterianimal/views.py
+++ PROYECTO/Veterianimal/Veterianimal/Veterianimal/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, request
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+from django.shortcuts import redirect
+from django.contrib import messages
+
 
 def index(request):
     return render(request, 'index.html', {
@@ -8,7 +14,28 @@ def index(request):
     })
 
 def login(request):
-    return render(request, 'login.html', {
-        'message': 'hola mundo',
-        'numbers': [1, 2, 3, 4, 5], 
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        print(username)
+        print(password)
+
+    return render(request, 'index.html',{
+
+    })
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, 'Bienvenido {}'.format(user.username))
+            return redirect('index')
+        else: 
+            messages.error(request, 'Usuario o contraseña incorrecta')
+    return render(request, 'users/login.html',{
+
     })
